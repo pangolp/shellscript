08fa68dd4afd96b9e7ab4b438d5c54c87f3834d1
pangolp <paganiwalter@gmail.com>
2013-09-06 20:42:51 -0300

    Practica N° 2

---
+++ TP2/script1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir $HOME/practicas/tp2 # Crea el directorio tp2, dentro de practicas, en el home del usuario
+echo "Estoy aprendiendo lenguaje scripting" # Imprimir el mensaje por pantalla
